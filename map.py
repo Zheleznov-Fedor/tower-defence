ed56839e01cf775e5618ad3e0fd574c3a74f6ffe
Fedor <fdjeleznov@gmail.com>
2022-12-31 17:50:51 +0300

    added map builder and tiles

--- map.py
+++ map.py
@@ -1,19 +1,7 @@
 import os
 import sys
 import pygame
-
-
-def parse_tile(code):
-    types = {
-        'r': '/road',
-        't': '/tower'
-    }
-    biomes = {
-        'd': '/desert',
-        's': '/spring',
-        'w': '/winter'
-    }
-    return types[code[0]] + biomes[code[1]] + '/' + code[2:4] + '.png'
+import csv
 
 
 def load_image(name, colorkey=None):
@@ -34,34 +22,153 @@ def load_image(name, colorkey=None):
     return image
 
 
+def parse_tile(code):
+    types = {
+        'R': '/road',
+        'D': '/decor',
+        'P': '/place'
+    }
+    biomes = {
+        'gg': '/grassgrass',
+        'dd': '/desertdesert',
+        'ss': '/stonestone',
+        'rr': '/redred',
+        'gd': '/grassdesert',
+        'gs': '/desertstone',
+        'gr': '/stonered',
+        'dg': '/desertgrass',
+        'ds': '/desertstone',
+        'dr': '/desertred',
+        'sg': '/stonegrass',
+        'sd': '/stonedesert',
+        'sr': '/stonered',
+        'rg': '/redgrass',
+        'rd': '/reddesert',
+        'rs': '/redstone'
+    }
+    materials = {
+        's': '/stone',
+        'g': '/grass'
+    }
+    place_materials = {
+        'gf': '/grassfill',
+        'gd': '/grassdirty',
+        'df': '/desertfill',
+        'dd': '/desertdirty'
+    }
+
+    if code[0] == 'R':
+        return types[code[0]] + biomes[code[1:3]] + '/' + str(int(code[3:5])) + '.png'
+    elif code[0] == 'D':
+        return types[code[0]] + materials[code[1]] + '/' + str(int(code[2:4])) + '.png'
+    elif code[0] == 'P':
+        return types[code[0]] + place_materials[code[1:3]] + '/' + str(int(code[3:5])) + '.png'
+
+
+def load_map(filname):
+    with open(filname, encoding="utf8") as file:
+        reader = csv.reader(file, delimiter=';', quotechar='"')
+        res = {
+            'land': [],
+            'decor': []}
+        r = list(reader)
+        w, h = map(int, r[0])
+
+        for i in range(1, h + 1):
+            res['land'].append(r[i + 1])
+
+        for elem in r[h + 2:]:
+            res['decor'].append({
+                'code': elem[0],
+                'x': float(elem[1]),
+                'y': float(elem[2]),
+            })
+
+        return res
+
+
+def build_map(filename, sprite_group):
+    map = load_map(filename)
+    land = map['land']
+    decor = map['decor']
+
+    for y in range(len(land)):
+        for x in range(len(land[0])):
+            Tile(sprite_group, land[y][x], x, y)
+
+    for elem in decor:
+        Tile(sprite_group, elem['code'], elem['x'], elem['y'])
+
+
 class Tile(pygame.sprite.Sprite):
-    def init(self, group, code, x, y):
-        super().init(group)
+    def __init__(self, group, code, x, y):
+        super().__init__(group)
         self.image = load_image('.' + parse_tile(code))
-        w = 100
-        h = int(0.94 * w)
-        self.image = pygame.transform.scale(self.image, (w, h))
+        size = 100
+        self.image = pygame.transform.scale(self.image, (size, size))
         self.rect = self.image.get_rect()
-        self.rect.x = w * x
-        self.rect.y = h * y
+        self.rect.x = size * x
+        self.rect.y = size * y
 
     def update(self, *args):
         pass
 
 
-if name == 'main':
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = load_image('./enemy/0.png')
+        size = 100
+        self.image = pygame.transform.scale(self.image, (size, size))
+        self.rect = self.image.get_rect()
+        self.start = (size * -0.5 - 24, size * 0.5)
+        self.rect.x = size * -0.5 - 24
+        self.rect.y = size * 0.5
+        self.path = [(1, 0, 0), (2, 0, 0), (3, 0, 0), (4, 0, 0), (4, 1, 1), (4, 2, 0)]
+        self.path_pos = 0
+        self.grid_pos = (0, 0)
+        print(self.grid_pos)
+
+    def update(self, *args):
+        if self.path_pos < len(self.path):
+            if self.rect.x - self.start[0] != 100 and self.rect.y - self.start[1] != 100:
+                if self.path[self.path_pos][0] > self.grid_pos[0]:
+                    self.rect.x += 1
+                elif self.path[self.path_pos][0] != self.grid_pos[0]:
+                    self.rect.x -= 1
+                if self.path[self.path_pos][1] > self.grid_pos[1]:
+                    self.rect.y += 1
+                elif self.path[self.path_pos][1] != self.grid_pos[1]:
+                    self.rect.y -= 1
+            else:
+                self.grid_pos = self.path[self.path_pos]
+                self.path_pos += 1
+                self.start = (self.rect.x, self.rect.y)
+                if self.path[self.path_pos][2]:
+                    self.image = pygame.transform.rotate(self.image, -90)
+                print(self.grid_pos)
+
+            # if self.rect.x - self.start[0] != 100:
+            #     self.rect.x += 1
+            # else:
+            #     self.grid_pos[0] += 1
+            #     self.path_pos += 1
+            #     self.start = (self.rect.x, self.rect.y)
+            #     print(self.grid_pos)
+
+
+if __name__ == '__main__':
     pygame.init()
-    screen = pygame.display.set_mode((500, 500))
+    screen = pygame.display.set_mode((600, 600))
     pygame.display.set_caption('Map test')
 
     running = True
-    fps = 60
+    fps = 120
     clock = pygame.time.Clock()
 
     all_sprites = pygame.sprite.Group()
 
-    Tile(all_sprites, 'rw07', 0, 0)
-    Tile(all_sprites, 'rw07', 1, 0)
+    build_map('test.csv', all_sprites)
 
     while running:
         for event in pygame.event.get():
@@ -72,11 +179,9 @@ if name == 'main':
 
         screen.fill((255, 255, 255))
 
-        all_sprites.update()
         all_sprites.draw(screen)
 
         clock.tick(fps)
         pygame.display.flip()
 
     pygame.quit()
-
